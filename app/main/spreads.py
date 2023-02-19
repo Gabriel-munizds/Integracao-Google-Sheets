2a329956ade825f695bd1eee6034531918a96862
Gabriel-munizds <gabrielmunizdasilva40@gmail.com>
2023-02-19 16:24:06 -0300

    envio de objeto df p/ o google sheets

--- app/main/spreads.py
+++ app/main/spreads.py
@@ -1,15 +1,27 @@
 import gspread
+from gspread_dataframe import set_with_dataframe
 from oauth2client.service_account import ServiceAccountCredentials
-
+import pandas as pd
 
 # Definindo Parâmetros da API e da planilha a ser trabalhada
 scope = ['https://spreadsheets.google.com/feeds'] # Escopo Utilizado
 credentials = ServiceAccountCredentials.from_json_keyfile_name('credentials/credentials.json', scope) # Dados de Autenticação
 gc = gspread.authorize(credentials) # Autenticação
-wks = gc.open_by_key('1yp2UhmO1HmSE-LjBFrLLxrT_33A_Q2gEMcs1B-rtrAk') # Abre a planilha a ser utilizada
-worksheet = wks.get_worksheet(0)
+id_sheet = '1Joar1DQpWKm2CSGaUYYtgmh3dS_TYJaO2OhZciIFzmo'
+wks = gc.open_by_key(id_sheet) # Abre a planilha a ser utilizada
+worksheet = wks.get_worksheet(1) # Seleciona a página (0,1,2...)
+
+# Criando Objeto DataFrame para ser inserido na planilha
+endereco_csv = 'database/vendas_01.01.2023_a_17.02.2023.csv'
+df = pd.read_csv(endereco_csv, sep=";", encoding='latin_1')
+df[' Valor Unit.'] = df[' Valor Unit.'].str.replace(",",".").astype(float)
+df[' Valor Total'] = df[' Valor Total'].str.replace(",", ".").astype(float)
+print(df.head())
+print(df.info())
 
 # inserindo dados
-worksheet.update_acell('A1', 'FUNCIONOU')
+set_with_dataframe(worksheet,df)
+print('Dados carregados com sucesso!')
+
 
 
